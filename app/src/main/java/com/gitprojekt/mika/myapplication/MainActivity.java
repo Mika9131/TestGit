5e3002405ea03920065da5d71ca685081a404952
mika <mikgunner@hotmail.de>
2018-08-12 14:33:34 +0200

    commit nummer 4

--- app/src/main/java/com/gitprojekt/mika/myapplication/MainActivity.java
+++ app/src/main/java/com/gitprojekt/mika/myapplication/MainActivity.java
@@ -3,11 +3,17 @@ package com.gitprojekt.mika.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.SQLOutput;
+
+// hallo dies ist ein test
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("TEST");
     }
 }
